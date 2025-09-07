ea8192f344e11f1eef4fc5cf9aa53886f101d7b8
Albright Sunguti <sungutialbright@gmail.com>
2025-09-07 17:40:38 +0300

    fix:change folder structure as per specs

--- alx_travel_app/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-from celery import Celery
-from django.conf import settings
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alx_travel_app.settings")
-
-app = Celery("alx_travel_app")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
